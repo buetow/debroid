a5542b650fb4bea4b9cf5bbd62bee7ebbbf18ba0
Paul Buetow (europa) <paul@buetow.org>
2015-06-20 12:46:31 +0100

    cleanup

--- data/local/debroid.sh
+++ data/local/debroid.sh
@@ -1,15 +1,11 @@
-
-log=/data/local/debroid.out
-err=/data/local/debroid.err
-
-/system/bin/date > $log
+#!/system/bin/sh
 
 while : ; do
   /system/xbin/mountpoint /storage/sdcard1
   if [ $? -eq 0 ]; then
     cd /storage/sdcard1/Linux
-    /system/bin/sh jessie.sh start_services >> $log 2> $err
-    /system/bin/date >> $log
+    /system/bin/sh jessie.sh start_services 
+    /system/bin/date
     exit 0
   fi
   /system/bin/sleep 1
